cae5044bc7f32d6627d1edf2bea56f7e47afe4df
Reap4ick <122297692+Reap4ick@users.noreply.github.com>
2025-01-08 15:10:51 +0200

    commit

--- products/urls.py
+++ products/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path("home/", views.home),
     path("list/", views.list),
     path("create/", views.create),
+    path("catalog/", views.catalog),
+    path("moredetail/<int:id>", views.moredetail, name = "moredetail"),
     path("edit/<int:id>", views.edit, name="edit"),
     path("details/<int:id>", views.details, name="details"),
     path("delete/<int:id>", views.delete, name="delete"),
--- products/views.py
+++ products/views.py
@@ -21,14 +21,22 @@ def details(request, id):
     try:
         product = Product.objects.get(pk=id)
         print(product)
-        return render(request, "detail.html", {"product": product})
+        return render(request, "detail.html", {"product": product,"return_url": "/products/list"})
     except:
         error="Product not except"
-        return render(request, "detail.html", {"error": error})
+        return render(request, "detail.html", {"error": error,"return_url": "/products/list"})
     
     # return render(request, "detail.html", {"product": product, "error": error})
 
-
+def moredetail(request, id):
+    try:
+        product = Product.objects.get(pk=id)
+        print(product)
+        return render(request, "moredetail.html", {"product": product,"return_url": "/products/catalog"})
+    except:
+        error="Product not except"
+        return render(request, "moredetail.html", {"error": error,"return_url": "/products/catalog"})
+    
 def delete(request, id):
     product = Product.objects.get(pk=id)
 
@@ -51,9 +59,11 @@ def create(request):
             form.save()
             return redirect("/products/list")
 
-    return render(request, "create.html", {"form": form})
+    return render(request, "create.html", {"form": form,"return_url": "/products/list"})
 
-
+def catalog(request):
+    products = Product.objects.all()
+    return render(request, "catalog.html", {"products": products})
 
 def edit(request, id):
 
@@ -71,4 +81,4 @@ def edit(request, id):
             form.save()
             return redirect("/products/list")
 
-    return render(request, "edit.html", {"form": form})
+    return render(request, "edit.html", {"form": form,"return_url": "/products/list"})
